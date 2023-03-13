ad6fcab678e80962b6c50ff5cffa194cc0bd19dd
cste0036 <cste0036@student.monash.edu>
2023-03-13 13:55:18 +1100

    Added Waste sorting information Pages

---
+++ FIT5120_WasteGuardian/Controllers/BinsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIT5120_WasteGuardian.Controllers
+{
+    [Authorize]
+    public class BinsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Recycling()
+        {
+            return View();
+        }
+
+        public IActionResult Landfill()
+        {
+            return View();
+        }
+
+        public IActionResult Organic()
+        {
+            return View();
+        }
+    }
+}
