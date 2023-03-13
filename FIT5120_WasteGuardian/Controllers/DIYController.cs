5402ebe83f2f8a66ac06d2cb7badc1768f4998a3
cste0036 <cste0036@student.monash.edu>
2023-03-13 22:05:15 +1100

    Added DIY pages

---
+++ FIT5120_WasteGuardian/Controllers/DIYController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIT5120_WasteGuardian.Controllers
+{
+    public class DIYController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Clothes()
+        {
+            return View();
+        }
+
+        public IActionResult Plastic()
+        {
+            return View();
+        }
+
+        public IActionResult Glass()
+        {
+            return View();
+        }
+    }
+}
