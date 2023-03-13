8dc193deda0a256ab0c704289995985c871325e7
cste0036 <cste0036@student.monash.edu>
2023-03-13 22:14:55 +1100

    Updated Homepage

--- FIT5120_WasteGuardian/Controllers/DIYController.cs
+++ FIT5120_WasteGuardian/Controllers/DIYController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FIT5120_WasteGuardian.Controllers
 {
+    [Authorize]
     public class DIYController : Controller
     {
         public IActionResult Index()
